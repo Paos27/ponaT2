8bba59f16903dc07b271c2a1f376da475f882f9a
pona <pona@SIST_02>
2024-04-15 17:44:15 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ponaT2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Vistas.vInicio(); //cAMBIANDO LA VENTANA DE EJECUCION 
+        }
+    }
+}
---
+++ Vistas/vInicio.xaml.cs
@@ -0,0 +1,9 @@
+namespace ponaT2.Vistas;
+
+public partial class vInicio : ContentPage
+{
+	public vInicio()
+	{
+		InitializeComponent();
+	}
+}
